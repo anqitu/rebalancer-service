ffa16bfdb2aa17f4fa1f81af6ccbb036a36f80df
Anqi Tu <anqitu@outlook.com>
2019-08-28 13:38:12 +0800

    Add budget and cost_coef to Setting

--- models/setting.py
+++ models/setting.py
@@ -1,6 +1,7 @@
 class Setting:
 
-    def __init__(self, interval_hour, peak_cost, off_peak_cost):
+    def __init__(self, interval_hour, peak_cost, off_peak_cost, budget_per_cycle, cost_coef):
         self.interval_hour = interval_hour
         self.peak_cost = peak_cost
         self.off_peak_cost = off_peak_cost
+        self.budget_per_cycle = budget_per_cycle
