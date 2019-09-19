bfc33ca19a8232a753d8a409bcb0fa7e431fd6e2
Anqi Tu <anqitu@outlook.com>
2019-09-19 12:46:26 +0800

    Create results folder

--- constants.py
+++ constants.py
@@ -9,10 +9,10 @@ STATUS_FINISH = 'finish'
 STATUS_NONE = None
 
 START_TIME = datetime(year = 2017, month = 9, day = 1, hour = 0)
-DEFAULT_SETTINGS = Settings(interval_hour = 2, peak_cost = 1, off_peak_cost = 1, budget_per_cycle = 200, cost_coef = 0.2)
+DEFAULT_SETTINGS = Settings(interval_hour = 2, peak_cost = 1, off_peak_cost = 1, budget_per_cycle = 1000, cost_coef = 0.2)
 MOVING_AVERAGE_DAYS = 7
 
-CYCLE_RESULTS_PATH = 'cycle_results.csv'
-SIMULATION_RESULT_PATH = 'simulation_result.csv'
-SETTING_PATH = 'setting.csv'
-SUPPLY_DEMAND_GAP_PATH = 'supply_demand_gap.csv'
+CYCLE_RESULTS_PATH = 'results/cycle_results.csv'
+SIMULATION_RESULT_PATH = 'results/simulation_result.csv'
+SETTING_PATH = 'results/setting.csv'
+SUPPLY_DEMAND_GAP_PATH = 'results/supply_demand_gap.csv'
